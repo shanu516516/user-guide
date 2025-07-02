e76a7ccc97069b34cb0dde9d9d8fbd85e8136faf
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2025-07-02 15:05:24 +0530

    fix 1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "twilight-project", // Usually your GitHub org/user name.
+  projectName: "user-guide", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
